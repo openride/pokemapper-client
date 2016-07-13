a09ef1c2f649765cbb08441db0d067adbcef48dc
phil <uniphil@gmail.com>
2016-07-13 01:04:58 -0400

    categorical colours

--- script.js
+++ script.js
@@ -154,9 +154,32 @@ message('Loading sighted pokemon...', function(closeMessage) {
         source: 'sightings',
         type: 'circle',
         paint: {
-          'circle-color': '#009b91',
+          'circle-color': {
+            property: 'type',
+            type: 'categorical',
+            stops: [
+              ['normal', '#A8A878'],
+              ['fighting', '#C03028'],
+              ['flying', '#A890F0'],
+              ['poison', '#A040A0'],
+              ['ground', '#E0C068'],
+              ['gock', '#B8A038'],
+              ['bug', '#A8B820'],
+              ['ghost', '#705898'],
+              ['steel', '#B8B8D0'],
+              ['fire', '#F08030'],
+              ['water', '#6890F0'],
+              ['grass', '#78C850'],
+              ['electric', '#F8D030'],
+              ['psychic', '#F85888'],
+              ['ice', '#98D8D8'],
+              ['dragon', '#7038F8'],
+              ['dark', '#705848'],
+              ['fairy', '#EE99AC'],
+            ]
+          },
           'circle-radius': 16,
-          'circle-opacity': 0.5,
+          'circle-opacity': 0.667,
         },
       });
     });
