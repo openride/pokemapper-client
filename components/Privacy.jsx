6b0bb3eeb49f6653b5c99e0b0fcac2c20600eb14
phil <uniphil@gmail.com>
2016-08-10 11:44:36 -0400

    Add a privacy policy

---
+++ components/Privacy.jsx
@@ -0,0 +1,114 @@
+import React from 'react';
+
+export default () => (
+  <div className="privacy">
+    <h2>Pokemapper Privacy Policy</h2>
+    <p>Welcome to Pokemapper!  Pokemapper Inc. (“Pokemapper, “we”, “us”, or “our”) has prepared this privacy policy to describe to you our practices regarding the information we collect from users of the pokemapper.co website, as well as the Pokemapper mobile applications, and Pokemapper’s services offered through the website and mobile applications (collectively, “Pokemapper Service”). By visiting this website or by using the mobile applications or otherwise accessing or using the Pokemapper Service, you are agreeing to the collection and use of your information in accordance with this Privacy Policy. If you do not agree with our information collection and use practices, please do not access or use the Pokemapper Service.</p>
+    <p>The Pokemapper Service is not directed to children and children are not eligible to use the Pokemapper Service.  Protecting the privacy of children is very important to us.  We do not collect or maintain personal information (i.e., information that allows someone to identify or contact an individual, including, for example, name, address, telephone number, e-mail address, as well as any other non-public information about an individual that is associated with or linked to any of the foregoing information) from people we actually know are under 13 years of age, and no part of our website, mobile applications, or Pokemapper Service, is designed to attract people under 13 years of age. If we later learn that a user is under 13 years of age, we will take steps to remove that user's personal information from our databases and to prevent the user from utilizing the Pokemapper Service.</p>
+    <p>Pokemapper respects the rights of its users.  Pokemapper collects the following information:</p>
+    <ul>
+      <li>
+        <p>Statistical information gathered automatically when you access the Pokemapper Service through the website or mobile application.</p>
+        <ul>
+          <li>Information Collected by Our Servers.  To make the Pokemapper Service more useful to you, our servers (which may be hosted by a third party service provider) collect information from you, including your computer when you use the Internet, which may vary from session to session), domain name, clickstream data, and a date/time stamp for your visit.</li>
+          <li>Log Files.  As is true of most websites, we gather certain information automatically and store it in log files.  This information includes IP addresses, browser type, Internet service provider (“ISP”), referring/exit pages, operating system, date/time stamp, and clickstream data.  We use this information to analyze trends, administer the Website, track users’ movements around the Website, gather demographic information about our user base as a whole, and better tailor the Pokemapper Service to our users’ needs.  For example, some of the information may be collected so that when you visit the Pokemapper Service again, it will recognize you and the information could then be used to serve advertisements and other information appropriate to your interests.</li>
+          <li>Cookies.  Like many online services, we use cookies to collect information.  “Cookies” are small pieces of information that a website sends to your computer’s hard drive while you are viewing the website.  We may use both session Cookies (which expire once you close your web browser) and persistent Cookies (which stay on your computer until you delete them) to provide you with a more personal and interactive experience on our Website.  This type of information is collected to make the Website more useful to you and to tailor the experience with us to meet your special interests and needs.</li>
+        </ul>
+      </li>
+      <li>
+        <p>Mobile Device. We may collect information about the computer or device you use to access the Pokemapper Service, including the hardware model, operating system and version, MAC address, unique device identifier, phone number, and mobile network information. In addition, the Pokemapper Service may access your device's image storage applications, with your consent, to facilitate your use of certain features of the Pokemapper Service. For more information about how you can control our access to these applications, please see "Your Choices" below.Information relating to your social networking or third-party accounts.</p>
+        <ul>
+          <li>We created interfaces that allow you to connect with other social networking websites including Facebook and Twitter (“Social Networking Sites”) through the Pokemapper Service, including as part of the registration process. We work with the Social Networking Site’s application protocol interface (better known as their developer API) in a way that allows you to authorize us to access your account on that Social Networking Site on your behalf.  In order to provide this authorization, you do not provide us with your user name or password to the Social Networking Site, but you will need to log-in to that Social Networking Site directly through the Pokemapper Service.  Once authorized by you, the Social Networking Site provides us a token that allows the Social Networking Site to recognize us when we ask, on your behalf, for access to your account information or to post information. For example, when you click “Connect” to grant Pokemapper access to your Facebook account, we will import your name, e-mail address, and profile photograph from Facebook, as well as other information. You can revoke our access to any Social Networking Site at any time by amending the appropriate settings from within your account settings on the applicable Social Networking Site.</li>
+        </ul>
+      </li>
+      <li>
+        <p>Information provided voluntarily in uploading a Pokemapper location.</p>
+      </li>
+      <li>
+        <p>When registering on our site, as appropriate, you will be asked to enter your name, e-mail address and/or phone number.</p>
+      </li>
+      <li>
+        <p>Geo-location information, as necessary to facilitate successful entry of Pokemon on the map.</p>
+      </li>
+      <li>
+        <p>Occasionally, we may conduct surveys to understand better the needs of our members in order to improve our features. We sometimes share the aggregated information in these surveys with partner organizations on an anonymized basis. We never share any information that could identify a specific user with any third party without that user’s express consent.</p>
+      </li>
+    </ul>
+
+    <h3>Use of data</h3>
+    <p>We will use your personal information to:</p>
+    <ul>
+      <li>Administer your account;</li>
+      <li>Provide you with the Pokemapper Service and customer support;</li>
+      <li>Tailor the features and content of the Pokemapper Service to you;</li>
+      <li>Market our products and services and those of third parties to you that we believe may be of interest to you;</li>
+      <li>Respond to your requests, resolve disputes and/or troubleshoot problems;</li>
+      <li>Verify your compliance with your obligations in our Terms of Use; and</li>
+      <li>Communicate with you about the Pokemapper Service.</li>
+    </ul>
+    <p>The information recorded in the website log files is used for statistical purposes only–including:</p>
+    <ul>
+      <li>to find out how many people are visiting the website</li>
+      <li>to find out how many people are visiting the website</li>
+      <li>to identify what pages are accessed most frequently</li>
+      <li>to help us improve the information we are supplying to our users</li>
+      <li>to make our site more user friendly</li>
+    </ul>
+    <p>An archive of these website log files and associated analysis and reports are retained indefinitely by Pokemapper as a way of measuring patterns of website traffic over the years and in order to facilitate future planning.</p>
+
+    <h3>Analytics</h3>
+    <p>This website uses web analytics services provided by analytics service providers, such as Google Analytics and Mixpanel. Analytics service providers use ‘cookies’, which are text files placed on your computer, to help the website analyze how users use the site. The information included in the cookie about your use of the website (including your IP address) will be transmitted to and stored by these services on servers in the United States. Analytics service providers use this information for the purpose of evaluating your use of the website, compiling reports on website activity for website operators and providing other services relating to website activity and internet usage. Analytics service providers may also transfer this information to third parties where required to do so by law, or where such third parties process the information on their behalf. Analytics service providers will not associate your IP address with any other data held by them. You may refuse the use of cookies by selecting the appropriate settings on your browser, however please note that if you do this you may not be able to use the full functionality of this website. By using this website, you consent to the processing of data about you by analytics service providers in the manner and for the purposes set out above.</p>
+
+    <h3>Storage of personal information</h3>
+    <p>Personal information submitted is kept on a server belonging to and administered by Pokemapper or an agent of Pokemapper which provides website hosting services and/or the email alert software. This agent of Pokemapper is compliant with the applicable data protection regulations and has provided written confirmation to Pokemapper that they will comply with the relevant data protection obligations by keeping information secure and not using it for any purpose other than for which it was provided. For backup purposes, copies of personal information, and subscriber lists are stored on secure servers to which agents of Pokemapper have remote access for the purposes of providing website development, maintenance or hosting services.</p>
+
+    <h3>Disclosure of personal information</h3>
+    <p>If a user joins a group their name, gender and trip details are available to that group. We do not sell, trade, or otherwise transfer your personally identifiable information to outside parties.</p>
+    <p>We may, however, disclose certain personal information as follows:</p>
+    <ul>
+      <li>
+        <p><strong>Third Parties Designated by You.</strong> When you use the Pokemapper Service, the personal information you provide will be shared with the third parties that you authorize to receive such information.</p>
+      </li>
+      <li>
+        <p><strong>Third Party Service Providers.</strong> We may share your personal information with our third party service providers that help us provide Pokemapper Service that we offer you.</p>
+      </li>
+      <li>
+        <p><strong>Affiliates.</strong> We may share some or all of your personal information with our parent company, subsidiaries, joint ventures, or other companies under a common control (“Affiliates”), if we have such Affiliates in the future, in which case we will require our Affiliates to honor this Privacy Policy.</p>
+      </li>
+      <li>
+        <p><strong>Corporate Restructuring.</strong> We may share some or all of your personal information in connection with or during negotiation of any merger, financing, acquisition or dissolution, transaction or proceeding involving sale, transfer, divestiture, or disclosure of all or a portion of our business or assets.  In the event of an insolvency, bankruptcy, or receivership, personal information may also be transferred as a business asset. If another company acquires our company, business, or assets, that company will possess the personal information collected by us and will assume the rights and obligations regarding your personal information as described in this Privacy Policy.</p>
+      </li>
+      <li>
+        <p><strong>Social Networking Sites.</strong> Our Pokemapper Service may enable you to post content to Social Networking Sites.  If you choose to do this, we will provide information to such Social Networking Sites in accordance with your elections.  You acknowledge and agree that you are solely responsible for your use of those websites and that it is your responsibility to review the terms of use and privacy policy of the third party provider of such Social Networking Site.  We will not be responsible or liable for: (i) the availability or accuracy of such Social Networking Sites; (ii) the content, products or services on or availability of such Social Networking Sites; or (iii) your use of any such Social Networking Sites.</p>
+      </li>
+      <li>
+        <p><strong>Other Disclosures.</strong> Regardless of any choices you make regarding your personal information, we may disclose personal information if it believes in good faith that such disclosure is necessary (a) in connection with any legal investigation; (b) to comply with relevant laws or to respond to subpoenas or warrants served on us; (c) to protect or defend our rights or property or other users of the Pokemapper Service; and/or (d) to investigate or assist in preventing any violation or potential violation of the law, this Privacy Policy, or our terms of use.</p>
+      </li>
+    </ul>
+
+    <h3>Security of your personal information</h3>
+    <p>Due to the nature of the internet we cannot guarantee the security of any information while it is being transmitted to us via the internet. However, we take our security responsibilities very seriously and take all reasonable steps (both technical and organizational) to protect your personal details once they have been received by Pokemapper.</p>
+
+    <h3>Your Choices regarding Your Information</h3>
+    <ul>
+      <li>Mobile Device. Certain features of the Pokemapper Service require access to your device's media storage and contacts applications. Although you may decide whether or not to use these features, on Android devices you grant us permission to access this feature as part of the installation process. If you later decide to revoke this permission, you can do so by following the standard uninstall process and removing the App from your device. Alternatively, on iOS devices, you can grant or revoke your consent at any time and prevent us from continuing to access your image storage applications by changing the settings on your device.</li>
+      <li>Email Communications. We may periodically send you free newsletters and e-mails that directly promote the use of the Pokemapper Service, or any part thereof.  When you receive newsletters or promotional communications from us, you may indicate a preference to stop receiving further communications from us and you will have the opportunity to “opt-out” by following the unsubscribe instructions provided in the e-mail you receive or by contacting us directly (please see contact information below). Despite your indicated e-mail preferences, we may send you service related communications, including notices of any updates to our terms of use or Privacy Policy, which you may not opt out of.</li>
+      <li>Cookies.  If you decide at any time that you no longer wish to accept cookies from our Pokemapper Service for any of the purposes described above, then you can instruct your browser, by changing its settings, to stop accepting cookies or to prompt you before accepting a cookie from the websites you visit. Consult your browser’s technical information. If you do not accept cookies, however, you may not be able to use all portions of the Pokemapper Service or all functionality of the Pokemapper Service. If you have any questions about how to disable or modify cookies, please let us know at the contact information provided below.</li>
+      <li>Changing or Deleting Your personal information. You may review, update, correct or delete the personal information in your account by contacting us at support@pokemapper.co. If you completely delete all of your personal information, then your account may become deactivated, although your profile and any content associated therewith will remain on the Pokemapper Service unless you have deleted them. We may also retain an archived copy of your records as required by law or for legitimate business purposes. Please remember, however, if we have already disclosed some of your personal information to third parties, we cannot access that personal information any longer and cannot force the deletion or modification of any such information by the parties to whom we have made those disclosures. Certain information is necessary in order for us to provide the Pokemapper Service; therefore, if you delete such necessary information you may not be able to use the Pokemapper Service.</li>
+    </ul>
+
+    <h3>Privacy statements on third party websites</h3>
+    <p>The Pokemapper Service may contain content or links to other websites that are not owned or controlled by us. We have no control over, do not review and are not responsible for the privacy policies of or content displayed on such other websites. When you click on such a link, you will leave the Pokemapper Service and go to another site.  During this process, another entity may collect personal information or anonymous information from you.</p>
+
+    <h3>Online Privacy Policy Only</h3>
+    <p>This Privacy Policy addresses only our use and disclosure of information we collect from and/or about you on the Pokemapper Service. If you disclose information to others, or authorize us to do the same under this Privacy Policy, the use and disclosure restrictions contained in this Privacy Policy will not apply to any third party. We do not control the privacy policies of third parties, and you are subject to the privacy policies of those third parties where applicable.</p>
+
+    <h3>Changes to our Privacy Policy</h3>
+    <p>This Privacy Policy may be updated from time to time for any reason.  We will notify you of any changes to our Privacy Policy by posting the new Privacy Policy here [hyperlink] and we will change the “Last Updated” date above.  You should consult this Privacy Policy regularly for any changes.</p>
+
+    <h3>Contacts</h3>
+    <p>Should you have any concerns about your privacy or any of the issues covered in this statement, please contact <a href="mailto:support@pokemapper.co">support@pokemapper.co</a>.</p>
+
+    <p>This document was last updated August 2016.</p>
+  </div>
+);
--- routes.js
+++ routes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Router, Route, browserHistory } from 'react-router';
 import Hello from './components/Hello';
+import Privacy from './components/Privacy';
 
 
 export default (
   <Router history={browserHistory}>
     <Route path="/" component={Hello} />
+    <Route path="/privacy" component={Privacy} />
   </Router>
 );
